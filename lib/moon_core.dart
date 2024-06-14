dc3a4f53b1bc9fc9fc09f1136332b6117b56ce94
Harry Sild <46851868+Kypsis@users.noreply.github.com>
2024-06-14 09:11:09 +0300

    BaseLayout Mix custom widget  (#1)
    * Finalize BaseLayout
    
    * Cleanup
    
    * More cleanup

--- lib/moon_core.dart
+++ lib/moon_core.dart
@@ -1,11 +1,9 @@
 /// Moon Design headless UI core
 library moon_core;
 
-export 'package:mix/mix.dart';
-
-export 'package:moon_core/src/utils/base_layout_widget.dart';
 export 'package:moon_core/src/utils/text_divider.dart';
 
 export 'package:moon_core/src/widgets/alert/alert.dart';
 export 'package:moon_core/src/widgets/alert/styled_alert.dart';
 export 'package:moon_core/src/widgets/button/button.dart';
+export 'package:moon_core/src/widgets/common/base_layout_widget/base_layout_widget.dart';
---
+++ lib/src/widgets/common/base_layout_widget/base_layout_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:mix/mix.dart';
+
+import 'package:moon_core/src/widgets/common/base_layout_widget/base_layout_spec.dart';
+
+class BaseLayoutWidget extends StyledWidget {
+  final Widget? leading;
+  final Widget? label;
+  final Widget? trailing;
+  final Widget? content;
+
+  const BaseLayoutWidget({
+    super.key,
+    super.style,
+    super.orderOfModifiers = const [],
+    this.leading,
+    this.label,
+    this.trailing,
+    this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SpecBuilder(
+      style: style,
+      orderOfModifiers: orderOfModifiers,
+      builder: (context) {
+        final baseLayoutStyle = BaseLayoutSpec.of(context);
+
+        final child = Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                if (leading != null) ...[
+                  leading!,
+                  SizedBox(width: baseLayoutStyle.horizontalGap),
+                ],
+                if (label != null)
+                  Expanded(
+                    child: switch (baseLayoutStyle.labelTextStyle != null) {
+                      true => DefaultTextStyle(
+                          style: baseLayoutStyle.labelTextStyle!,
+                          child: label!,
+                        ),
+                      _ => label!,
+                    },
+                  ),
+                if (trailing != null) ...[
+                  SizedBox(width: baseLayoutStyle.horizontalGap),
+                  trailing!,
+                ],
+              ],
+            ),
+            if (content != null) ...[
+              SizedBox(height: baseLayoutStyle.verticalGap),
+              switch (baseLayoutStyle.contentTextStyle != null) {
+                true => DefaultTextStyle(
+                    style: baseLayoutStyle.contentTextStyle!,
+                    child: content!,
+                  ),
+                _ => content!,
+              },
+            ],
+          ],
+        );
+
+        return baseLayoutStyle.defaultIconStyle != null
+            ? IconTheme(
+                data: baseLayoutStyle.defaultIconStyle!,
+                child: child,
+              )
+            : child;
+      },
+    );
+  }
+}
