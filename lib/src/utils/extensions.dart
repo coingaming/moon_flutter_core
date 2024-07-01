3cdb39dec3e3271d1d82ef58527364b8568dc772
Harry Sild <46851868+Kypsis@users.noreply.github.com>
2024-07-01 03:32:50 +0300

    Cleanup

--- lib/src/utils/extensions.dart
+++ lib/src/utils/extensions.dart
@@ -1,48 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:mix/mix.dart';
 
 import 'package:moon_core/src/mix/modifiers/default_text_style_widget_modifier.dart';
 import 'package:moon_core/src/mix/modifiers/icon_theme_widget_modifier.dart';
-
-extension IconThemeDataX on IconThemeData {
-  bool get hasAValue =>
-      color != null ||
-      size != null ||
-      shadows != null ||
-      fill != null ||
-      weight != null ||
-      grade != null ||
-      opticalSize != null ||
-      applyTextScaling != null;
-}
-
-extension TextStyleX on TextStyle {
-  bool get hasAValue =>
-      background != null ||
-      backgroundColor != null ||
-      color != null ||
-      debugLabel != null ||
-      decoration != null ||
-      decorationColor != null ||
-      decorationStyle != null ||
-      decorationThickness != null ||
-      fontFamily != null ||
-      fontFamilyFallback != null ||
-      fontFeatures != null ||
-      fontSize != null ||
-      fontStyle != null ||
-      fontVariations != null ||
-      fontWeight != null ||
-      foreground != null ||
-      height != null ||
-      leadingDistribution != null ||
-      letterSpacing != null ||
-      locale != null ||
-      overflow != null ||
-      shadows != null ||
-      textBaseline != null ||
-      wordSpacing != null;
-}
 
 extension WithModifierUtilityX on WithModifierUtility {
   static final _textExpando = Expando<DefaultTextStyleWidgetUtility>();
