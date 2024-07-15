c9e3f6e267c1136321eac3ca8e2294e7a0998585
Hub <79840500+GittHub-d@users.noreply.github.com>
2024-07-15 17:21:38 +0300

    Segmented tab control component (#6)
    Co-authored-by: Birgitt Majas <birgitt.majas@yolo.com>

---
+++ lib/src/widgets/segmented_tab_control/segmented_tab_control.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:mix/mix.dart';
+import 'package:moon_core/moon_core.dart';
+import 'package:moon_core/src/widgets/segmented_tab_control/segmented_tab.dart';
+
+class MoonRawSegmentedTabControl extends StatefulWidget {
+  /// Axis direction of the segmented tab control.
+  final Axis axisDirection;
+
+  /// Whether the segmented tab control is disabled.
+  final bool isDisabled;
+
+  /// Whether the segmented tab control is expanded to its full available width
+  /// horizontally.
+  final bool isExpanded;
+
+  /// The index of the initially selected segmented tab.
+  /// This property is overridden by the [tabController]'s initial index if the
+  /// [tabController] is provided.
+  /// For external updates to the segmented tab index, use [tabController].
+  final int initialIndex;
+
+  /// Style of the segmented tab control container.
+  final Style? style;
+
+  /// The external controller for managing segmented tab selection and animation
+  /// in segmented tab control. If [tabController] is provided, then [initialIndex]
+  /// is ignored and [tabController]'s index is used instead.
+  final TabController? tabController;
+
+  /// The callback that returns the index of the currently selected segmented tab.
+  final ValueChanged<int>? onTabChanged;
+
+  /// The list of segmented tabs to display as the children of the segmented
+  /// tab control. At least one child is required.
+  final List<MoonRawSegmentedTab> tabs;
+
+  /// Creates a Moon Design raw segmented tab control.
+  const MoonRawSegmentedTabControl({
+    super.key,
+    this.axisDirection = Axis.horizontal,
+    this.isDisabled = false,
+    this.isExpanded = false,
+    this.initialIndex = 0,
+    this.style,
+    this.tabController,
+    this.onTabChanged,
+    required this.tabs,
+  }) : assert(tabs.length > 0);
+
+  @override
+  State<MoonRawSegmentedTabControl> createState() =>
+      _MoonRawSegmentedTabControlState();
+}
+
+class _MoonRawSegmentedTabControlState extends State<MoonRawSegmentedTabControl>
+    with SingleTickerProviderStateMixin {
+  late int _selectedIndex;
+  late TabController _controller;
+
+  void _handleTabChange() {
+    final int newIndex = _controller.index;
+
+    if (newIndex != _selectedIndex) {
+      setState(() {
+        _selectedIndex = newIndex;
+        widget.onTabChanged?.call(newIndex);
+
+        _updateTabsSelectedStatus();
+      });
+    }
+  }
+
+  void _updateTabsSelectedStatus() {
+    widget.tabs.asMap().forEach((int index, MoonRawSegmentedTab tab) {
+      tab.isSelected?.call(index == _selectedIndex);
+    });
+  }
+
+  void _handleTap(int index) {
+    if (index >= 0 && index < widget.tabs.length) {
+      _controller.index = index;
+
+      widget.onTabChanged?.call(index);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = widget.tabController ??
+        DefaultTabController.maybeOf(context) ??
+        TabController(
+          vsync: this,
+          length: widget.tabs.length,
+          initialIndex: widget.initialIndex,
+        );
+
+    _controller.animation?.addListener(_handleTabChange);
+
+    _selectedIndex = _controller.index;
+  }
+
+  @override
+  void dispose() {
+    _controller.animation?.removeListener(_handleTabChange);
+
+    if (widget.tabController == null) _controller.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> children = List.generate(
+      widget.tabs.length,
+      (int index) {
+        final child = MoonBaseInteractiveWidget(
+          enabled: !widget.isDisabled && !widget.tabs[index].disabled,
+          enableFeedback: widget.tabs[index].enableFeedback,
+          autofocus: widget.tabs[index].autoFocus,
+          focusNode: widget.tabs[index].focusNode,
+          semanticLabel: widget.tabs[index].semanticLabel,
+          style: widget.tabs[index].tabStyle,
+          onFocusChange: widget.tabs[index].onFocusChange,
+          onPress: () => _handleTap(index),
+          child: widget.tabs[index].child!,
+        );
+
+        return widget.isExpanded ? Expanded(child: child) : child;
+      },
+    );
+
+    return widget.axisDirection == Axis.horizontal
+        ? HBox(
+            style: widget.style,
+            children: children,
+          )
+        : VBox(
+            style: widget.style,
+            children: children,
+          );
+  }
+}
---
+++ lib/src/widgets/segmented_tab_control/styled_segmented_tab_control.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:mix/mix.dart';
+import 'package:moon_core/src/widgets/common/variants/selected_state_variants.dart';
+import 'package:moon_core/src/widgets/segmented_tab_control/segmented_tab.dart';
+import 'package:moon_core/src/widgets/segmented_tab_control/segmented_tab_control.dart';
+
+class StyledSegmentedTabControl extends StatefulWidget {
+  const StyledSegmentedTabControl({super.key});
+
+  @override
+  State<StyledSegmentedTabControl> createState() =>
+      _StyledSegmentedTabControlState();
+}
+
+class _StyledSegmentedTabControlState extends State<StyledSegmentedTabControl>
+    with SingleTickerProviderStateMixin {
+  late TabController tabController = TabController(length: 3, vsync: this);
+
+  final Duration _duration = const Duration(milliseconds: 200);
+
+  int _selectedIndex = 0;
+
+  SelectedState _getVariant(int index) => index == _selectedIndex
+      ? SelectedState.selected
+      : SelectedState.unselected;
+
+  @override
+  Widget build(BuildContext context) {
+    final Style pillTabContainerStyle = Style(
+      $box.color(Colors.grey.shade300),
+      $box.borderRadius(12),
+      $box.padding(4),
+      $flex.gap(4.0),
+      $flex.mainAxisSize.min(),
+    );
+
+    final Style pillTabStyle = Style(
+      $box.borderRadius(8),
+      $box.padding(8.0, 16.0),
+      $text.style.color(Colors.black),
+      ($on.hover | $on.focus | $on.press | $on.longPress)(
+        $box.color(Colors.purple.shade400),
+        $text.style.color(Colors.white),
+      ),
+      $on.disabled(
+        $box.color(Colors.transparent),
+        $text.style.color(Colors.black),
+        $with.opacity(0.5),
+      ),
+      SelectedState.selected(
+        $box.color(Colors.purple.shade400),
+        $text.style.color(Colors.white),
+      ),
+    );
+
+    final Style indicatorTabStyle = Style(
+      $box.height(32),
+      $text.style.color(Colors.black),
+      ($on.hover | $on.focus | $on.press | $on.longPress)(
+        $text.style.color(Colors.deepPurple),
+      ),
+      $on.disabled(
+        $with.opacity(0.5),
+        $text.style.color(Colors.black),
+      ),
+      SelectedState.selected(
+        $text.style.color(Colors.deepPurple),
+      ),
+    );
+
+    Style getIndicatorStyle(double width) => Style(
+          $box.width(0),
+          $box.height(2),
+          $box.color(Colors.deepPurple),
+          $with.align(alignment: Alignment.bottomLeft),
+          ($on.hover | $on.focus | $on.press | $on.longPress)(
+            $box.width(width),
+          ),
+          $on.disabled(
+            $box.width(0),
+          ),
+          SelectedState.selected(
+            $box.width(width),
+          ),
+        );
+
+    return Column(
+      children: [
+        MoonRawSegmentedTabControl(
+          style: pillTabContainerStyle,
+          onTabChanged: (int index) => setState(() => _selectedIndex = index),
+          tabs: List.generate(
+            3,
+            (int index) => MoonRawSegmentedTab(
+              tabStyle: pillTabStyle
+                  .animate(duration: _duration)
+                  .applyVariant(_getVariant(index)),
+              child: StyledText('Tab ${index + 1}'),
+            ),
+          ),
+        ),
+        const SizedBox(height: 24),
+        MoonRawSegmentedTabControl(
+          isExpanded: true,
+          tabController: tabController,
+          style: Style(
+            $flex.gap(4.0),
+          ),
+          onTabChanged: (int index) => setState(() => _selectedIndex = index),
+          tabs: List.generate(
+            3,
+            (int index) => MoonRawSegmentedTab(
+              tabStyle: indicatorTabStyle
+                  .animate(duration: _duration)
+                  .applyVariant(_getVariant(index)),
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  StyledText('Tab ${index + 1}'),
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    child: LayoutBuilder(
+                      builder: (context, constraints) {
+                        return Box(
+                          style: getIndicatorStyle(constraints.maxWidth)
+                              .animate(duration: _duration)
+                              .applyVariant(_getVariant(index)),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 80,
+          child: TabBarView(
+            controller: tabController,
+            children: List.generate(
+              3,
+              (int index) => ColoredBox(
+                color: Colors.deepPurpleAccent.shade100,
+                child: Center(
+                  child: Text('Tab ${index + 1} content'),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
