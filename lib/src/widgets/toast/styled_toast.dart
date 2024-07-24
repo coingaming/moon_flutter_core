982abce67e84f71701ffc3208bc974f10edd8ea8
Hub <79840500+GittHub-d@users.noreply.github.com>
2024-07-24 11:11:57 +0300

    Toast base component (#8)
    Co-authored-by: Birgitt Majas <birgitt.majas@yolo.com>

---
+++ lib/src/widgets/toast/styled_toast.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:mix/mix.dart';
+import 'package:moon_core/moon_core.dart';
+
+class StyledToast extends StatelessWidget {
+  const StyledToast({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MoonBaseInteractiveWidget(
+      style: Style(
+        $box.color(Colors.deepPurpleAccent),
+        $box.padding(8, 16),
+        $box.borderRadius(8),
+      ),
+      onPress: () => MoonRawToast.show(
+        context,
+        style: Style(
+          $box.color(Colors.black87),
+          $box.margin(16),
+          $box.padding(8, 16),
+          $box.borderRadius(8),
+          $text.style.color(Colors.white70),
+          $flex.gap(8),
+          $flex.mainAxisSize.min(),
+        ),
+        child: const StyledText('This is toast content!'),
+      ),
+      child: const Text('Show toast'),
+    );
+  }
+}
