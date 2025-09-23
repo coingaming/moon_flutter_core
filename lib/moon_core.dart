f105878dc0c6af0ea7ba7a9a26e5bc44d7d7ee34
Harry Sild <46851868+Kypsis@users.noreply.github.com>
2025-09-23 17:11:19 +0300

    remove redundant custom implementations

--- lib/moon_core.dart
+++ lib/moon_core.dart
@@ -5,7 +5,6 @@ export 'package:moon_core/src/mix/attributes/moon_border_dto.dart';
 export 'package:moon_core/src/mix/context_variants/active_state_variant.dart';
 
 export 'package:moon_core/src/utils/color/color_tween_premul.dart';
-export 'package:moon_core/src/utils/extensions.dart';
 export 'package:moon_core/src/utils/methods.dart';
 export 'package:moon_core/src/utils/moon_border.dart';
 export 'package:moon_core/src/utils/shape_decoration_premul.dart';
